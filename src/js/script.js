173597d948f69829eb0e6ffb3235773205ae0f10
Jaden Wang <jadenyjw@gmail.com>
2017-12-21 10:39:59 -0500

    Add textbox

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,23 @@
 const GITHUB_LINK = "https://github.com/jadenyjw"
 const LINKEDIN_LINK = "https://linkedin.com/in/jadenyjw/"
 const RESUME_LINK = "resume.pdf"
+var current_dir = "~"
+var command = ""
+
+
+$(document).ready(function(){
+    window.addEventListener("keypress", myEventHandler, false);
+    document.getElementById('line').focus();
+});
+
+function myEventHandler(e){
+    var keyCode = e.keyCode;
+    console.log(keyCode);
+    if(keyCode == 13){
+
+    }
+};
+
+function processCommand(command){
+
+};
